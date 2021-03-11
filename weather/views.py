7cd97b76208304e4a40b3ae57ee177059decc551
visrut <visrutnavadiya@gmail.com>
2021-03-11 09:41:27 +0530

    show weather data in table

--- weather/views.py
+++ weather/views.py
@@ -4,5 +4,5 @@ from . import weather_data
 # Create your views here.
 
 def weather_info(request):
-	weather_table = weather_data.get_weather_data()
-	return render(request,"weather/weather_info.html",{"weather_table":weather_table})
+	weather_report = weather_data.get_weather_data()
+	return render(request,"weather/weather_info.html",{"weather_table":weather_report})
--- weather/weather_data.py
+++ weather/weather_data.py
@@ -4,19 +4,24 @@ import requests
 def get_weather_data():
 
 	# url to take live weather data
-	url = "https://www.timeanddate.com/weather/?continent=asia"
+	url = "https://www.weather-forecast.com/countries/India-1"
 
 	# get webpage source
 	source = requests.get(url).text
 	soup = BeautifulSoup(source,'lxml')
 
 	# get weather_table
-	weather_table = soup.findAll('table')
-	weather_table = weather_table[0]
+	weather_table = soup.find(class_="top-city-list-per-country")
+	weather_table = weather_table.find_all('li')
 
-	weather_table.findAll('tr')
+	weather_report = []
+	for li in weather_table:
+		country_name = li.a.text
+		weather_info = li.div.img['alt']
+		weather_report.append((country_name,weather_info))
 
-	return weather_table
+	# print(weather_report)
+	return weather_report
 
 if __name__ == '__main__':
 	get_weather_data()
